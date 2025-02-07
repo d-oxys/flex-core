1a63c2dd3c4fa2f7a584ec913848a19be5ec6999
d-oxys <mochamadichsannr@gmail.com>
2025-02-07 10:02:07 +0700

    ops: ci test

---
+++ src/pages/cicd/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/rootLayout";
+import LandingPage from "@/components/landingPage";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="bg-white">
+        <p className="text-red-500 text-2xl">CICD</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
